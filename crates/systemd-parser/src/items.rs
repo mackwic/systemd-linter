18591e478d21c3f21f142229aecfcd4299227adf
Thomas Wickham <twickham@octo.com>
2017-01-11 14:39:04 +0100

    parser should be finished now

--- crates/systemd-parser/src/items.rs
+++ crates/systemd-parser/src/items.rs
@@ -23,35 +23,128 @@ impl<'a> UnitDirective<'a> {
             key: key,
         }
     }
+
+    pub fn item_list_to_unit_directive_list(unit_items: &'a Vec<SystemdItem<'a>>)
+        -> Result<Vec<UnitDirective<'a>>, ()> {
+
+        use self::SystemdItem::*;
+
+        if unit_items.len() < 1 {
+            return Err(()) // TODO: error message
+        }
+
+        let mut cat = try!(UnitDirective::get_first_category(unit_items));
+        let mut res = vec!();
+
+        for item in unit_items {
+            match *item {
+                Category(new_cat) => cat = new_cat,
+                Directive(key, value) => res.push(UnitDirective::new(cat, key, value)),
+                _ => () // TODO: do something with comments ?
+            }
+        }
+
+        Ok(res)
+    }
+
+    fn get_first_category<'b>(unit_items: &'b Vec<SystemdItem<'b>>) -> Result<&'b str, ()> {
+        if let Some(&SystemdItem::Category(first_cat)) = unit_items.get(0) {
+            Ok(first_cat)
+        } else {
+            return Err(()) // TODO: error message
+        }
+    }
 }
 
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct SystemdUnit<'a> {
-    directives: HashMap<&'a str, UnitDirective<'a>>
+    directives: HashMap<&'a str, DirectiveEntry<'a>>
+}
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub enum DirectiveEntry<'a> {
+    Solo(UnitDirective<'a>),
+    Many(Vec<UnitDirective<'a>>)
 }
 
 impl<'a> SystemdUnit<'a> {
 
-    pub fn new(unit_items: Vec<SystemdItem>) -> Result<SystemdUnit, ()> {
+    pub fn new(unit_items: &'a Vec<SystemdItem<'a>>) -> Result<SystemdUnit<'a>, ()> {
 
-        if false == SystemdUnit::first_is_category(unit_items) {
-            // TODO: error message
-            return Err(())
-        }
+        let directives = try!(
+            UnitDirective::item_list_to_unit_directive_list(&unit_items)
+        );
+
+        let mut directives_hash = try!(
+            SystemdUnit::hash_from_directives(directives)
+        );
 
         let res = SystemdUnit {
-            directives: HashMap::new()
+            directives: directives_hash
         };
         Ok(res)
     }
 
+    fn hash_from_directives(directives: Vec<UnitDirective>) -> Result<HashMap<&str, DirectiveEntry>, ()> {
 
-    fn first_is_category(unit_items: Vec<SystemdItem>) -> bool {
-        match unit_items.get(0) {
-            Some(&SystemdItem::Category(_)) => true,
-            _ => false
+        use self::DirectiveEntry::*;
+        use std::collections::hash_map::Entry;
+
+        let mut directives_hash = HashMap::new();
+
+        for directive in directives {
+            match directives_hash.entry(directive.key) {
+                // first entry is a Solo
+                Entry::Vacant(mut entry) => { entry.insert(Solo(directive)); },
+                Entry::Occupied(mut entry) => {
+                    let mut entry = entry.get_mut();
+                    match *entry {
+                // 2nd entry is a conversion from Solo to Many
+                        Solo(first_dir) => {
+                            let dirs = vec!(first_dir, directive);
+                            try!(SystemdUnit::validate_many(&dirs));
+                            *entry = Many(dirs);
+                        }
+                // 3rd+ entry is an append to vec in Many(vec)
+                        Many(ref mut dirs) => {
+                            dirs.push(directive);
+                            try!(SystemdUnit::validate_many(dirs));
+                        },
+                    }
+                }
+            }
+        }
+
+        Ok(directives_hash)
+    }
+
+    fn validate_many(dirs: &Vec<UnitDirective>) -> Result<(), ()> {
+        // contract: invariant
+        assert!(dirs.len() >= 2);
+
+        let first = dirs.first().unwrap();
+        let last = dirs.last().unwrap();
+        if first.category == last.category {
+            Ok(())
+        } else {
+            Err(()) // TODO: error message
         }
     }
-}
 
+    pub fn lookup_by_key(&self, key: &str) -> Option<&DirectiveEntry> {
+        self.directives.get(key)
+    }
+
+    pub fn lookup_by_category(&self, category: &str) -> Vec<&DirectiveEntry> {
+		use self::DirectiveEntry::*;
+
+        self.directives
+            .values()
+            .filter(|directive| match **directive { 
+				Solo(dir) => dir.category == category,
+				Many(ref dirs) => category == dirs.get(0).expect("dirs.len() > 0").category,
+			})
+            .collect()
+    }
+}
 
--- crates/systemd-parser/src/items_test.rs
+++ crates/systemd-parser/src/items_test.rs
@@ -1,9 +1,115 @@
 
 pub use items::*;
+pub use items::SystemdItem::*;
+pub use items::DirectiveEntry::*;
+
+mod unit_directive {
+    pub use super::*;
+
+    mod item_list_to_unit_directive_list {
+        pub use super::*;
+
+        #[test]
+        fn it_should_err_for_empty_vecs() {
+            let input = vec!();
+            let res = UnitDirective::item_list_to_unit_directive_list(&input);
+
+            assert!(res.is_err());
+        }
+
+        #[test]
+        fn it_should_instantiate_a_unit_directive_from_category_and_directive_items() {
+            let input = vec![
+                Category("Unit"),
+                Directive("Description", "plop"),
+            ];
+            let res = UnitDirective::item_list_to_unit_directive_list(&input);
+            let expected = UnitDirective::new("Unit", "Description", "plop");
+
+            assert!(res.is_ok());
+            assert_eq!(Some(&expected), res.unwrap().get(0))
+        }
+
+        #[test]
+        fn the_second_directive_should_keep_the_category() {
+            let input = vec![
+                Category("Unit"),
+                Directive("Description", "plop"),
+                Directive("Wants", "boot.target"),
+            ];
+            let res = UnitDirective::item_list_to_unit_directive_list(&input);
+            let expected = UnitDirective::new("Unit", "Wants", "boot.target");
+
+            assert!(res.is_ok());
+            assert_eq!(Some(&expected), res.unwrap().get(1))
+        }
+
+        #[test]
+        fn it_should_change_the_category_if_a_category_item_is_seen() {
+            let input = vec![
+                Category("Unit"),
+                Directive("Description", "plop"),
+                Category("Service"),
+                Directive("ExecStart", "/usr/bin/true"),
+            ];
+            let res = UnitDirective::item_list_to_unit_directive_list(&input);
+            let expected = UnitDirective::new("Service", "ExecStart", "/usr/bin/true");
+
+            assert!(res.is_ok());
+            assert_eq!(Some(&expected), res.unwrap().get(1))
+        }
+
+        #[test]
+        fn it_should_err_if_the_first_item_is_not_a_category() {
+            let input = vec![
+                Directive("Description", "plop"),
+                Category("Service"),
+                Directive("ExecStart", "/usr/bin/true"),
+            ];
+            let res = UnitDirective::item_list_to_unit_directive_list(&input);
+
+            assert!(res.is_err());
+        }
+
+        #[test]
+        fn it_should_work_with_full_dummy_unit() {
+            let input = vec![
+                Category("Unit"),
+                Directive("Description", "Some HTTP server"),
+                Directive("After", "remote-fs.target sqldb.service memcached.service"),
+                Directive("Requires", ""),
+                Directive("Requires","sqldb.service memcached.service"),
+                Directive("AssertPathExists","/srv/www"),
+                Category("Service"),
+                Directive("Type", "notify"),
+                Directive("ExecStart", "/usr/sbin/some-fancy-httpd-server"),
+                Directive("Nice", "0"),
+                Directive("PrivateTmp", "yes"),
+                Category("Install"),
+                Directive("WantedBy", "multi-user.target"),
+            ];
+            let res = UnitDirective::item_list_to_unit_directive_list(&input);
+
+            let expected = vec![
+                UnitDirective::new("Unit", "Description", "Some HTTP server"),
+                UnitDirective::new("Unit", "After", "remote-fs.target sqldb.service memcached.service"),
+                UnitDirective::new("Unit", "Requires", ""),
+                UnitDirective::new("Unit", "Requires","sqldb.service memcached.service"),
+                UnitDirective::new("Unit", "AssertPathExists","/srv/www"),
+                UnitDirective::new("Service", "Type", "notify"),
+                UnitDirective::new("Service", "ExecStart", "/usr/sbin/some-fancy-httpd-server"),
+                UnitDirective::new("Service", "Nice", "0"),
+                UnitDirective::new("Service", "PrivateTmp", "yes"),
+                UnitDirective::new("Install", "WantedBy", "multi-user.target"),
+            ];
+
+            assert_eq!(expected, res.unwrap());
+        }
+    }
+}
 
 mod systemd_unit {
     pub use super::*;
-    pub use super::SystemdItem::*;
 
     mod new {
         pub use super::*;
@@ -15,7 +121,7 @@ mod systemd_unit {
                 Directive("Description", "A dummy unit file"),
             ];
 
-            let res = SystemdUnit::new(input);
+            let res = SystemdUnit::new(&input);
 
             assert!(res.is_ok())
         }
@@ -26,9 +132,134 @@ mod systemd_unit {
                 Directive("Description", "A dummy unit file"),
             ];
 
-            let res = SystemdUnit::new(input);
+            let res = SystemdUnit::new(&input);
 
             assert!(res.is_err())
+        }
+
+        #[test]
+        fn it_should_err_when_many_directives_of_same_key_are_in_different_categories() {
+            // arrange
+            let input = vec![
+                Category("Service"),
+                Directive("ExecStartPre", "/usr/bin/true"),
+                Category("Install"),
+                Directive("ExecStartPre", "/usr/bin/true"),
+            ];
+            // act
+            let res = SystemdUnit::new(&input);
+            // assert
+            assert!(res.is_err());
+        }
+
+        #[test]
+        fn it_should_err_when_many_directives_of_same_key_are_in_different_categories2() {
+            // arrange
+            let input = vec![
+                Category("Service"),
+                Directive("ExecStartPre", "/usr/bin/true"),
+                Directive("ExecStartPre", "/usr/bin/true"),
+                Category("Install"),
+                Directive("ExecStartPre", "/usr/bin/true"),
+            ];
+            // act
+            let res = SystemdUnit::new(&input);
+            // assert
+            assert!(res.is_err());
+        }
+    }
+
+    mod lookup_by_key {
+        pub use super::*;
+
+        #[test]
+        fn it_should_return_the_unit_directive_pointed_by_key() {
+            // arrange
+            let input = vec![
+                Category("Unit"),
+                Directive("Description", "A dummy unit file"),
+            ];
+            let unit = SystemdUnit::new(&input).unwrap();
+            let directive = Solo(UnitDirective::new("Unit", "Description", "A dummy unit file"));
+            let expected = Some(&directive);
+            // act
+            let res = unit.lookup_by_key("Description");
+            // assert
+            assert_eq!(expected, res);
+        }
+
+        #[test]
+        fn it_should_return_none_when_inexistent() {
+            // arrange
+            let input = vec![
+                Category("Service"),
+                Directive("ExecStartPre", "/usr/bin/true"),
+            ];
+            let unit = SystemdUnit::new(&input).unwrap();
+            let expected = None;
+            // act
+            let res = unit.lookup_by_key("Description");
+            // assert
+            assert_eq!(expected, res);
+        }
+
+        #[test]
+        fn it_should_return_a_vec_of_all_the_keys_when_multiple_keys() {
+            // arrange
+            let input = vec![
+                Category("Service"),
+                Directive("ExecStartPre", "/usr/bin/true"),
+                Directive("ExecStartPre", "/usr/bin/true"),
+                Directive("ExecStartPre", "/usr/bin/true"),
+            ];
+            let unit = SystemdUnit::new(&input).unwrap();
+            let directive = UnitDirective::new("Service", "ExecStartPre", "/usr/bin/true");
+            let expected = Many(vec![
+                directive,
+                directive,
+                directive,
+            ]);
+            // act
+            let res = unit.lookup_by_key("ExecStartPre");
+            // assert
+            assert_eq!(Some(&expected), res);
+        }
+    }
+
+    mod lookup_by_category {
+        pub use super::*;
+
+        #[test]
+        fn it_should_exists() {
+            // arrange
+            let input = vec![
+                Category("Unit"),
+                Directive("Description", "A dummy unit file"),
+            ];
+            let unit = SystemdUnit::new(&input).unwrap();
+            let directive = Solo(UnitDirective::new("Unit", "Description", "A dummy unit file"));
+            let expected = vec![
+                &directive
+            ];
+            // act
+            let res = unit.lookup_by_category("Unit");
+            // assert
+            assert_eq!(expected, res);
+        }
+
+        #[test]
+        fn it_should_return_an_empty_vec_the_category_doesnt_exists() {
+            // arrange
+            let input = vec![
+                Category("Unit"),
+                Directive("Description", "A dummy unit file"),
+            ];
+            let unit = SystemdUnit::new(&input).unwrap();
+            let expected : Vec<&DirectiveEntry> = vec![];
+            // act
+            let res = unit.lookup_by_category("Service");
+            // assert
+            assert_eq!(expected, res);
         }
     }
 }
